4f42f57873a0d06294617c608fa505e66571d28a
lvantuire <lvantuire@gmail.com>
2022-05-10 14:36:52 -0300

    fim da primeira parte da primeira aula

--- lesson-01/src/App.js
+++ lesson-01/src/App.js
@@ -4,8 +4,7 @@ function App() {
   return (<div>
     <h1>Olá Mundo
     </h1>
-    <p>teste de teste
-    </p>
+    
     </div>)
 
 }
--- lesson-01/src/index.js
+++ lesson-01/src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function soma(a,b) {
+  return ("a soma de A + B é   " + (a+b))
+}
+
+function primeiroJSX() {
+  return (
+    <div className="teste">
+      Luiz Vantuir - Introdução ao ReactJs
+    <h3>{soma(10,11)}</h3></div>
+  )
+
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {primeiroJSX()}
   </React.StrictMode>
 );
 
